5b225ff51802c79ac0e97da832b88f0d7eeac8d3
Larissa <larissa.o.santana@aluno.senai.br>
2024-11-13 09:06:23 -0300

    Adicionar arquivos de projeto.

---
+++ Eletrodomesticos/Controllers/ListaEletrodomesticosController.cs
@@ -0,0 +1,20 @@
+﻿using Eletrodomesticos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eletrodomesticos.Controllers
+{
+    public class ListaEletrodomesticosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Eletrodomestico> eletrodomesticos = new List<Eletrodomestico>()
+            {
+                new Geladeira { Marca = "Brastemp", Modelo = "Brastemp Ative!", ConsumoEnergetico = 0.74, CapacidadeLitros = 375, TemFreezer = true },
+                new Televisor { Marca = "Lg", Modelo = "Alexa", ConsumoEnergetico = 500, TamanhoTela = 75 },
+                new Geladeira { Marca = "Samsung", Modelo = "Duplex", ConsumoEnergetico = 3500, CapacidadeLitros = 500, TemFreezer = false },
+                new Televisor { Marca = "Samsung", Modelo = "Samsung QLED 4K - Q60C", ConsumoEnergetico = 90 - 130, TamanhoTela = 65 }
+            };
+            return View(eletrodomesticos);
+        }
+    }
+}
---
+++ Eletrodomesticos/Models/Televisor.cs
@@ -0,0 +1,12 @@
+﻿namespace Eletrodomesticos.Models
+{
+        public class Televisor : Eletrodomestico
+        {
+            public int TamanhoTela { get; set; }
+            public bool EhSmart { get; set; }
+            public override string ExibirDetalhes()
+            {
+                return $"Marca: {Marca}, Modelo: {Modelo}, Consumo: {ConsumoEnergetico} kWh, Tamanho da tela em polegadas: {TamanhoTela}, A TV é uma Smart TV? {(EhSmart ? "Sim" : "Não")}";
+            }
+        }
+    }
